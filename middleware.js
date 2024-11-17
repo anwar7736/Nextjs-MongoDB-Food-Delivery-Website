ddd04bb2d9a069970eb707071e28681a465d75d9
Md. Anwar Hossain <anwarhossain7736@gmail.com>
2024-11-17 09:25:33 +0600

    Something changes in middleware

---
+++ middleware.js
@@ -0,0 +1,25 @@
+import { NextResponse } from 'next/server';
+
+export function middleware(request) {
+  const token = request.cookies.get('restaurant_auth')?.value;
+  const AuthTryToAccess = request.nextUrl.pathname == '/restaurant';
+  if(AuthTryToAccess)
+  {
+    if(token)
+    {
+      return NextResponse.redirect(new URL('/restaurant/dashboard', request.url));
+    }
+  }
+  else{
+    if(!token)
+      {
+        return NextResponse.redirect(new URL('/restaurant', request.url));
+      }
+  }
+  
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ['/restaurant', '/restaurant/dashboard'],
+};
